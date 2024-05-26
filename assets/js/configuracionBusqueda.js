6fab84e5e63aa33047db401482db2ab55bfd5606
David Villen Moreno <dvillenmoreno@gmail.com>
2024-05-26 04:04:55 +0200

    ss

--- assets/js/configuracionBusqueda.js
+++ assets/js/configuracionBusqueda.js
@@ -1,21 +1,17 @@
+// Definir la configuración para SimpleJekyllSearch
 var searchConfig = {
   resultsContainer: document.getElementById('results-container'),
   json: idioma === "es" ? '/assets/json/search_es.json' : '/assets/json/search_en.json'
 };
 
+// Inicializar SimpleJekyllSearch con la configuración definida
 var sjs = new SimpleJekyllSearch(searchConfig);
 
-function buscarYRedirigir() {
-  var query = document.getElementById('q').value;
-  var results = obtenerResultadosDeBusqueda(searchTerm);
-  localStorage.setItem('searchResults', JSON.stringify(results));
-  // Redireccionar a la página de búsqueda
-  if(idioma==="es"){
-    window.location.href = baseUrl + "/search.html";
-  }else{
-    window.location.href = baseUrl +"/search_en.html";
-  }
-  if (query) {
-      sjs.search(query);
-  }
+// Función para obtener resultados de búsqueda
+function obtenerResultadosDeBusqueda(searchTerm) {
+  var results = [];
+  sjs.search(searchTerm, function(result) {
+      results.push(result);
+  });
+  return results;
 }
