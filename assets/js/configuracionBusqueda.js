920bb0b6fdb0ed2658ef485f0f766632a78b442b
David Villen Moreno <dvillenmoreno@gmail.com>
2024-05-26 03:56:31 +0200

    xx

--- assets/js/configuracionBusqueda.js
+++ assets/js/configuracionBusqueda.js
@@ -1,12 +1,21 @@
 var searchConfig = {
-    searchInput: document.getElementById('q'),
-    resultsContainer: document.getElementById('results-container')
-  };
+  resultsContainer: document.getElementById('results-container'),
+  json: idioma === "es" ? '/assets/json/search_es.json' : '/assets/json/search_en.json'
+};
 
-  if (idioma === "es") {
-    searchConfig.json = '/assets/json/search_es.json';
-  } else {
-    searchConfig.json = '/assets/json/search_en.json';
+var sjs = new SimpleJekyllSearch(searchConfig);
+
+function buscarYRedirigir() {
+  var query = document.getElementById('q').value;
+  var results = obtenerResultadosDeBusqueda(searchTerm);
+  localStorage.setItem('searchResults', JSON.stringify(results));
+  // Redireccionar a la página de búsqueda
+  if(idioma==="es"){
+    window.location.href = baseUrl + "/search.html";
+  }else{
+    window.location.href = baseUrl +"/search_en.html";
   }
-
-new SimpleJekyllSearch(searchConfig);
+  if (query) {
+      sjs.search(query);
+  }
+}
